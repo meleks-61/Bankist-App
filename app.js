b58d1964e5c8ab825cf00fd2afe7cc5d7e1ae699
meleks-61 <maibalonn@gmail.com>
2022-07-28 17:44:52 +0300

    dom elements created

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
 // Data
-// const account1 = {
-//     owner: 'Jonas Schmedtmann',
-//     movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-//     interestRate: 1.2, // %
-//     pin: 1111,
-//   };
+const account1 = {
+    owner: 'Jonas Schmedtmann',
+    movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+    interestRate: 1.2, // %
+    pin: 1111,
+  };
   
-//   const account2 = {
-//     owner: 'Jessica Davis',
-//     movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-//     interestRate: 1.5,
-//     pin: 2222,
-//   };
+  const account2 = {
+    owner: 'Jessica Davis',
+    movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+    interestRate: 1.5,
+    pin: 2222,
+  };
   
-//   const account3 = {
-//     owner: 'Steven Thomas Williams',
-//     movements: [200, -200, 340, -300, -20, 50, 400, -460],
-//     interestRate: 0.7,
-//     pin: 3333,
-//   };
+  const account3 = {
+    owner: 'Steven Thomas Williams',
+    movements: [200, -200, 340, -300, -20, 50, 400, -460],
+    interestRate: 0.7,
+    pin: 3333,
+  };
   
-//   const account4 = {
-//     owner: 'Sarah Smith',
-//     movements: [430, 1000, 700, 50, 90],
-//     interestRate: 1,
-//     pin: 4444,
-//   };
+  const account4 = {
+    owner: 'Sarah Smith',
+    movements: [430, 1000, 700, 50, 90],
+    interestRate: 1,
+    pin: 4444,
+  };
   
-//   const accounts = [account1, account2, account3, account4];
+  const accounts = [account1, account2, account3, account4];
   
 //   /////////////////////////////////////////////////
 //   // Elements
@@ -225,3 +225,40 @@
 //     displayMovements(currentAccount.movements, !sorted);
 //     sorted = !sorted;
 //   });
+
+//Elements
+
+const loginEl=document.querySelector(".login__btn")
+const userInputEl=document.querySelector(".login__input--user")
+
+
+
+const createUserName= (accs)=>{
+    accs.forEach((acc) => {
+        acc.userName=acc.owner
+        .toLowerCase()
+        .split(" ")
+        .map(e=>e[0])
+        .join("")
+      
+    
+        
+    });
+
+}
+createUserName(accounts)
+accounts.forEach(e=>console.log(e.userName))
+
+
+
+
+
+
+let currentAcc;
+loginEl.addEventListener("click", (e)=>{
+    e.preventDefault()
+    currentAcc=accounts.find((e)=>e.userName===userInputEl.value.toLowerCase())
+    if (currentAcc) console.log(`hello ${currentAcc.owner}`)
+    
+
+})
